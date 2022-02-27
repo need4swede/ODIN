38389cf41470c17548e2a65a9a98d46b2e882c07
Need4Swede <theneed4swede@gmail.com>
2022-02-27 11:01:25 -0800

    added scrolling to nav colors

--- Mimir/Documentation/js/main.js
+++ Mimir/Documentation/js/main.js
@@ -3,11 +3,19 @@ const navLinks = document.querySelectorAll('.nav-link');
 const mainNavLink = document.querySelector('.main-nav-link');
 mainNavLink.addEventListener('click', highlightNavMenu);
 Array.from(navLinks).forEach(element => element.addEventListener('click', highlightNavMenu));
-function giveNavColorsBlack(moduleName){
+function addNavColorsBlack(moduleName){
+    document.querySelector(`.nav-${moduleName}`).classList.add('color-black');
+    document.querySelector(`.nav-link-${moduleName}`).classList.add(`give-nav-${moduleName}-background-color`);
+}
+function addNavColorsWhite(moduleName){
+    document.querySelector(`.nav-${moduleName}`).classList.add('color-white');
+    document.querySelector(`.nav-link-${moduleName}`).classList.add(`give-nav-${moduleName}-background-color`);
+}
+function toggleNavColorsBlack(moduleName){
     document.querySelector(`.nav-${moduleName}`).classList.toggle('color-black');
     document.querySelector(`.nav-link-${moduleName}`).classList.toggle(`give-nav-${moduleName}-background-color`);
 }
-function giveNavColorsWhite(moduleName){
+function toggleNavColorsWhite(moduleName){
     document.querySelector(`.nav-${moduleName}`).classList.toggle('color-white');
     document.querySelector(`.nav-link-${moduleName}`).classList.toggle(`give-nav-${moduleName}-background-color`);
 }
@@ -21,25 +29,61 @@ function resetNavColorsWhite(moduleName){
 }
 function highlightNavMenu(click){
     if(click.target.classList.contains('nav-odin')){
-        giveNavColorsWhite('odin');
+        toggleNavColorsWhite('odin');
         resetNavColorsBlack('tyr');
         resetNavColorsWhite('mimir');
         resetNavColorsWhite('heimdall');
     }
     if(click.target.classList.contains('nav-tyr')){
-        giveNavColorsBlack('tyr');
+        toggleNavColorsBlack('tyr');
         resetNavColorsWhite('mimir');
         resetNavColorsWhite('heimdall');
     }
     if(click.target.classList.contains('nav-mimir')){
-        giveNavColorsWhite('mimir');
+        toggleNavColorsWhite('mimir');
         resetNavColorsBlack('tyr');
         resetNavColorsWhite('heimdall');
     }
     if(click.target.classList.contains('nav-heimdall')){
-        giveNavColorsWhite('heimdall');
+        toggleNavColorsWhite('heimdall');
         resetNavColorsBlack('tyr');
         resetNavColorsWhite('mimir');
     }
 }
 
+// NAVIGATION HIGHLIGHTING / VIEWPORT ELEMENT DETECTION
+const functionTyr = document.getElementById('jump-tyr');
+const functionMimir = document.getElementById('jump-mimir');
+const functionHeimdall = document.getElementById('jump-heimdall');
+const functionIntro = document.getElementById('end-intro');
+let isInViewport = function (elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+window.addEventListener('scroll', function (event) {
+    if (isInViewport(functionIntro)) {
+        resetNavColorsBlack('tyr');
+        resetNavColorsWhite('heimdall');
+        resetNavColorsWhite('mimir');
+	}
+	if (isInViewport(functionTyr)) {
+        addNavColorsBlack('tyr');
+        resetNavColorsWhite('mimir');
+        resetNavColorsWhite('heimdall');
+	}
+    if (isInViewport(functionMimir)) {
+        addNavColorsWhite('mimir');
+        resetNavColorsBlack('tyr');
+        resetNavColorsWhite('heimdall');
+	}
+    if (isInViewport(functionHeimdall)) {
+        addNavColorsWhite('heimdall');
+        resetNavColorsBlack('tyr');
+        resetNavColorsWhite('mimir');
+	}
+}, false);
