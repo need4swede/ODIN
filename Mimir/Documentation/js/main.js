17bb4241c8b4fd728cf2076c0a4c03e1e5c93d3c
Need4Swede <theneed4swede@gmail.com>
2022-02-27 10:17:43 -0800

    navhighlighting for main link

--- Mimir/Documentation/js/main.js
+++ Mimir/Documentation/js/main.js
@@ -1,6 +1,8 @@
 // NAVIGATION HIGHLIGHTING
 const navLinks = document.querySelectorAll('.nav-link');
-Array.from(navLinks).forEach(element => element.addEventListener('click', highlightNavMenu))
+const mainNavLink = document.querySelector('.main-nav-link');
+mainNavLink.addEventListener('click', highlightNavMenu);
+Array.from(navLinks).forEach(element => element.addEventListener('click', highlightNavMenu));
 function giveNavColorsBlack(moduleName){
     document.querySelector(`.nav-${moduleName}`).classList.toggle('color-black');
     document.querySelector(`.nav-link-${moduleName}`).classList.toggle(`give-nav-${moduleName}-background-color`);
@@ -18,6 +20,12 @@ function resetNavColorsWhite(moduleName){
     document.querySelector(`.nav-link-${moduleName}`).classList.remove(`give-nav-${moduleName}-background-color`);
 }
 function highlightNavMenu(click){
+    if(click.target.classList.contains('nav-odin')){
+        giveNavColorsWhite('odin');
+        resetNavColorsBlack('tyr');
+        resetNavColorsWhite('mimir');
+        resetNavColorsWhite('heimdall');
+    }
     if(click.target.classList.contains('nav-tyr')){
         giveNavColorsBlack('tyr');
         resetNavColorsWhite('mimir');
