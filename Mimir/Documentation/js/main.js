16f516e054f0fa4383eb6c5b9e9b5edc0f0adbeb
Need4Swede <theneed4swede@gmail.com>
2022-02-27 10:07:56 -0800

    added highlighting to nav elements

--- Mimir/Documentation/js/main.js
+++ Mimir/Documentation/js/main.js
@@ -1,1 +1,37 @@
+// NAVIGATION HIGHLIGHTING
+const navLinks = document.querySelectorAll('.nav-link');
+Array.from(navLinks).forEach(element => element.addEventListener('click', highlightNavMenu))
+function giveNavColorsBlack(moduleName){
+    document.querySelector(`.nav-${moduleName}`).classList.toggle('color-black');
+    document.querySelector(`.nav-link-${moduleName}`).classList.toggle(`give-nav-${moduleName}-background-color`);
+}
+function giveNavColorsWhite(moduleName){
+    document.querySelector(`.nav-${moduleName}`).classList.toggle('color-white');
+    document.querySelector(`.nav-link-${moduleName}`).classList.toggle(`give-nav-${moduleName}-background-color`);
+}
+function resetNavColorsBlack(moduleName){
+    document.querySelector(`.nav-${moduleName}`).classList.remove('color-black');
+    document.querySelector(`.nav-link-${moduleName}`).classList.remove(`give-nav-${moduleName}-background-color`);
+}
+function resetNavColorsWhite(moduleName){
+    document.querySelector(`.nav-${moduleName}`).classList.remove('color-black');
+    document.querySelector(`.nav-link-${moduleName}`).classList.remove(`give-nav-${moduleName}-background-color`);
+}
+function highlightNavMenu(click){
+    if(click.target.classList.contains('nav-tyr')){
+        giveNavColorsBlack('tyr');
+        resetNavColorsWhite('mimir');
+        resetNavColorsWhite('heimdall');
+    }
+    if(click.target.classList.contains('nav-mimir')){
+        giveNavColorsWhite('mimir');
+        resetNavColorsBlack('tyr');
+        resetNavColorsWhite('heimdall');
+    }
+    if(click.target.classList.contains('nav-heimdall')){
+        giveNavColorsWhite('heimdall');
+        resetNavColorsBlack('tyr');
+        resetNavColorsWhite('mimir');
+    }
+}
 
